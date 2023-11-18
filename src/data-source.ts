6b5883c8370d755ff4d4c5b26572599255a08cea
Victor Medeiros <victormedeiros.pj@gmail.com>
2023-11-18 16:09:59 -0300

    fix .env

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,11 +1,12 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Item } from "./entity/Item";
+require("dotenv").config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DATABASE_HOST,
-  port: 5432,
+  port: parseInt(process.env.DATABASE_PORT),
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
